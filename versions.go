99e98e5f4101f174516015b9d4e64c928dbfaa79
torbenmconto <torbenmconto@gmail.com>
2024-02-22 22:20:03 -0500

    Fix messed up url

--- versions.go
+++ versions.go
@@ -11,7 +11,7 @@ func NewVersion(name, path, url string, badLines []string) *Version {
 	return &Version{name, path, url, badLines}
 }
 
-var KJV = *NewVersion("kjv", "kjv.txt", "https://raw.githubusercontent.com/scrollmapper/bible_databases/master/csv/t_kjv.csv", []string{"KJV", "King James Bible: Pure Cambridge Edition - Text courtesy of www.BibleProtector.com"})
+var KJV = *NewVersion("kjv", "kjv.txt", "https://openbible.com/textfiles/kjv.txt", []string{"KJV", "King James Bible: Pure Cambridge Edition - Text courtesy of www.BibleProtector.com"})
 
 var versions = []Version{
 	KJV,
