dad6729762a3aa774bec5e6845bf5d195795caa5
torbenmconto <torbenmconto@gmail.com>
2024-02-24 01:19:39 -0500

    Better init process, concurrency, progress bar

--- init.go
+++ init.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func InitDotBible() {
+
 	home, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
@@ -20,6 +27,7 @@ func InitDotBible() {
 	_, err = os.Stat(baseDir)
 	if err != nil {
 		if os.IsNotExist(err) {
+			fmt.Println("Initializing .bible...")
 			err = os.Mkdir(baseDir, 0755)
 			if err != nil {
 				log.Fatal(err)
@@ -32,42 +40,19 @@ func InitDotBible() {
 			}
 
 			for _, v := range versions {
-				// Create baseDir/versions/{version.name}
-				versionDir := filepath.Join(versionsDir, v.Name)
-				os.Mkdir(versionDir, 0755)
-
-				// Download the version
-				resp, err := http.Get(v.Url)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				split := strings.Split(v.Url, ".")
-				name := filepath.Join(versionDir, v.Name+"."+split[len(split)-1])
-				file, err := os.Create(name)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				// Write the file
-				_, err = io.Copy(file, resp.Body)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				if err := file.Close(); err != nil {
-					log.Fatal(err)
-				}
-
-				if err := resp.Body.Close(); err != nil {
-					log.Fatal(err)
-				}
+				wg.Add(1)
+				go InitVersion(v)
 			}
+			wg.Wait()
+			fmt.Println(".bible initialization completed.")
 		}
 	}
+
 }
 
 func InitVersion(version Version) {
+	defer wg.Done()
+
 	home, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
@@ -75,42 +60,42 @@ func InitVersion(version Version) {
 
 	baseDir := filepath.Join(home, ".bible")
 	versionsDir := filepath.Join(baseDir, "versions")
+
+	// Create baseDir/versions/{version.name}
 	versionDir := filepath.Join(versionsDir, version.Name)
+	os.Mkdir(versionDir, 0755)
 
-	_, err = os.Stat(versionDir)
+	// Download the version
+	resp, err := http.Get(version.Url)
 	if err != nil {
-		if os.IsNotExist(err) {
-			err = os.Mkdir(versionDir, 0755)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// Download the version
-			resp, err := http.Get(version.Url)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			split := strings.Split(version.Url, ".")
-			name := filepath.Join(versionDir, version.Name+"."+split[len(split)-1])
-			file, err := os.Create(name)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// Write the file
-			_, err = io.Copy(file, resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if err := file.Close(); err != nil {
-				log.Fatal(err)
-			}
-
-			if err := resp.Body.Close(); err != nil {
-				log.Fatal(err)
-			}
-		}
+		log.Fatal(err)
 	}
+	defer resp.Body.Close()
+
+	size, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
+
+	split := strings.Split(version.Url, ".")
+	name := filepath.Join(versionDir, version.Name+"."+split[len(split)-1])
+	file, err := os.Create(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Initialize progress bar
+	bar := pb.Full.Start(size)
+
+	// Create a proxy reader to track progress
+	reader := bar.NewProxyReader(resp.Body)
+
+	// Write the file
+	_, err = io.Copy(file, reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Finish the progress bar
+	bar.Finish()
+
+	log.Printf("Version %s initialization completed.\n", version.Name)
 }
